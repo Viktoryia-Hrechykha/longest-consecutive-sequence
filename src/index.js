c4362ccf3e3ed024ba80947cdeaeefd963e199a6
Viktoryia_Hrechykha <viktoryia_hrechykha@epam.com>
2018-10-08 19:35:15 +0300

    answ

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+  let sortedArray = [];
+  let sequences = [];
+  let prev = 0;
+  let maxLength = 1;
+
+  if (!array.length) return 0;
+
+  if (array.length === 1) return 1;
+
+  array.forEach((el) => {
+      if (el > sortedArray[sortedArray.length - 1]) {
+          sortedArray.push(el)
+      } else {
+          sortedArray.splice(sortedArray[sortedArray.length - 2], 0, el)
+      }
+  });
+
+  sortedArray.forEach((el) => {
+      if (el - prev !== 1) sequences.push([]);
+      sequences[sequences.length - 1].push(el);
+      prev = el;
+  });
+
+  for (let i = 0; i < sequences.length; i++) {
+      if (sequences[i - 1] && sequences[i].length > sequences[i - 1].length && sequences[i].length > maxLength) maxLength = sequences[i].length;
+  }
+
+  return maxLength;
+};
